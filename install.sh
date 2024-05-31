ca184bd515ea1d9a6533bd0c74d4f1d9ae26af6a
ellipse12 <ellipse1234567@gmail.com>
2024-05-30 21:21:05 -0600

    Updated installer

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 path="$HOME/.pack"
 mkdir "$path"
 cp pack.py "$path"
-echo "export PATH=$path/pack:PATH" > "$HOME/.profile"
-echo "#! /usr/bin/env bash" >> "$path/pack"
-echo "dir=\"\$(dirname \"\$0\")\"" > "$path/pack"
-echo "python3 \$dir/pack.py -f \$HOME/.pack/pack.toml \"\$@\""
-echo "[pack]" >> $path/pack.toml
+echo "export PATH=$path:\$PATH" >> "$HOME/.profile"
+echo "#! /usr/bin/env bash" > "$path/pack"
+echo "dir=\"\$(dirname \"\$0\")\"" >> "$path/pack"
+echo "cd \"\$dir\"" >> "$path/pack"
+echo "python3 \$dir/pack.py \"\$@\"" >> "$path/pack"
+echo "[pack]" > $path/pack.toml
+chmod u+x "$path/pack"
 echo "Done!"
